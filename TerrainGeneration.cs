9a2f27e666d8385aa2960f8798d3818d2602f207
Jimmy Spearman <spearmanjim@yahoo.com>
2015-12-28 19:52:22 -0500

    added files

---
+++ TerrainGeneration.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainGeneration : MonoBehaviour {
+	public int MAX_CHUNKS_X = 5;
+	public int MAX_CHUNKS_Y = 5;
+	public int CHUNK_SIZE
+	private Terrain[][] chunkMap;
+
+	// Use this for initialization
+	void Start () {
+		//chunkMap = new Terrain[MAX_CHUNKS_X] [MAX_CHUNKS_Y];
+
+	
+	}
+	
+	// Update is called once per frame. Future use for loading in new area maybe?
+	//void Update () {
+	//
+	//}
+}
